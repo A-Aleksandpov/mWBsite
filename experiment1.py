1f3bcdc32af21cf3e7a9f047127183faf54c376f
A-Aleksandpov <117276628+A-Aleksandpov@users.noreply.github.com>
2023-09-24 15:21:10 +0300

    Create experiment1.py
    basic test

---
+++ experiment1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+#need photos
+st.set_page_config(page_title="Experimental Webpage", page_icon=":tada:", layout="wide")
+
+#head
+st.subheader ("Hi, i am Anton insert text")
+st.title ("A programmer From Russia")
+st.write("I am passionate about finding ways to use Python in Data analytics")
+st.write("[Learn More >](https://github.com/A-Aleksandpov)")
+
