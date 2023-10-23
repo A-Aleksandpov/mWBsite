6ff861ecbd106ad1b888f5b6053ffe3b0627af84
A-Aleksandpov <117276628+A-Aleksandpov@users.noreply.github.com>
2023-10-23 21:39:26 +0300

    Update experiment1.py

--- experiment1.py
+++ experiment1.py
@@ -14,6 +14,58 @@ st.title ("A programmer From Russia")
 st.write("I am passionate about finding ways to use Python in Data analytics")
 st.write("[Learn More >](https://github.com/A-Aleksandpov)")
 
+#who am i
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("Who am i")
+        st.write("##")
+        st.write(
+            """
+            
+            - I realized that I want to become a Python programmer.
+            - I am interested in all possible areas where this language can be applied, in particular, data science and machine learning. I am open to learning new technologies.
+            - Touching and participating in the development of large and interesting projects is what really got me interested in programming. I am focused on solving problems and implementing quality and beautiful code using modern design standards.
+            - Soft skills: multitasking, teamwork, time management.
+            - My main focus is Python; SQL; Django; Pytorch; Pyspark; MLflow; Golang, Matplotlib, Numpy, Scipy, Streamlit, Random, Tkinter, PyQt5, math, pandas, sklearn, seaborn."
+           """
+        )
+
+#other info
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("My pet project and other skills")
+        st.write("##")
+        st.write(
+            """
+            
+            * Program to collect data from website using bs4, numpy, space, trafilatura, to csv file requests.
+            * Program to analyze excel file using numpy, pandas, seaborn, sklearn, xgboost.
+            * Program to transfer file to google cloud. Using pydrive, google-cloud-storage, pandas modules.
+            * Personal website.
+            * Browser using PyQt5 module.
+            * Notepad using tkinter module.
+            * Rock, scissors, paper game with the help of tkinter, random modules.
+            * Engineering calculator using tkinter, pygame, speechrecognition modules. Responds to voice commands.
+            * Tetris with pygame, math, random, pathlib, sys.
+            * Chat via socket module.
+            * 3D snake with ursina and random modules.
+            * Gaming machine with the help of random module.
+
+                    Skills and abilities
+            * Project management: Github;
+            Text editors and integrated development environment: Microsoft Word, MS Visual Studio Code, docker, MS Office.
+
+           """
+        )
+
+ 
+
+
+
 #What i do
 with st.container():
     st.write("---")
@@ -47,3 +99,8 @@ with st.container():
         <button type="submit">Send</button>
     </form>
     """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
