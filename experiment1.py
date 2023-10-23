b36d94fe8b9b4494c0cb1176deaff3815ae8e200
A-Aleksandpov <117276628+A-Aleksandpov@users.noreply.github.com>
2023-10-23 21:17:04 +0300

    Update experiment1.py

--- experiment1.py
+++ experiment1.py
@@ -2,13 +2,14 @@ import requests
 import streamlit as st
 from PIL import Image
 
-wave_img = ImageTk.PhotoImage(Image.open("wave1.png"))
+#doesnt work with currect free website
+#wave_img = ImageTk.PhotoImage(Image.open("wave1.png"))
 
 #need photos
 st.set_page_config(page_title="Experimental Webpage", page_icon=":tada:", layout="wide")
 
 #head
-st.subheader ("Hi, i am Anton wave_img")
+st.subheader ("Hi, i am Anton Aleksandrov.") #should be img
 st.title ("A programmer From Russia")
 st.write("I am passionate about finding ways to use Python in Data analytics")
 st.write("[Learn More >](https://github.com/A-Aleksandpov)")
