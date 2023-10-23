5345e2a48765f1c06e3ac30ea1f51c81a9e5cbe6
A-Aleksandpov <117276628+A-Aleksandpov@users.noreply.github.com>
2023-10-23 20:20:25 +0300

    Update experiment1.py

--- experiment1.py
+++ experiment1.py
@@ -6,7 +6,7 @@ wave_img = ImageTk.PhotoImage(Image.open("wave1.png"))
 st.set_page_config(page_title="Experimental Webpage", page_icon=":tada:", layout="wide")
 
 #head
-st.subheader ("Hi, i am Anton insert text")
+st.subheader ("Hi, i am Anton wave_img")
 st.title ("A programmer From Russia")
 st.write("I am passionate about finding ways to use Python in Data analytics")
 st.write("[Learn More >](https://github.com/A-Aleksandpov)")
