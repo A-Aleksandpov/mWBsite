6546a3e395c64e6157bd3ffe8caf3b20a40f6d58
A-Aleksandpov <117276628+A-Aleksandpov@users.noreply.github.com>
2023-10-24 20:49:23 +0300

    Update experiment1.py

--- experiment1.py
+++ experiment1.py
@@ -93,7 +93,7 @@ with st.container():
     st.write("##")
 
     contact_form = """
-    <form action="https://formsubmit.co/A.Aleksandpov@yandex.ru" method="POST">
+    <form action="https://formsubmit.co/tiavmo@gmail.com" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
